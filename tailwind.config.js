74d71f58c12b26b95fcd88920d89004003e9ab19
siddharthpelkhn <109087112+siddharthpelkhn@users.noreply.github.com>
2023-07-07 18:22:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors:{
+    deepBlue: "#02042a",
+    lightBlue: "#2b84ea",
+    lightBlue500: "#0b72e7",
+    grayBlue: "#344a6c",
+    LightBlue300: "#4b94ed",
+    greenLight: "#61cea6",
+    grayText: "#818597",
+    LightGray: "#e2e2e2",
+    deepBlueHead: "#162156",
+    gray2: "#525a76",
+    }
+  },
+},
+  plugins: [],
+}
